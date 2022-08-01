14420bc0b9329825b02cc83bfeb832b52a3e4675
Quan <quan@gmail.com>
2022-08-01 10:03:59 +0700

    lastest update

--- js/ProductManagement.js
+++ js/ProductManagement.js
@@ -1,16 +1,16 @@
-class productManagement {
-    constructor(array) {
-        this.Product = array;
-    }
-    showList () {
-        let sp = "";
-            sp = <div class="col-4">
-            sp = <div class="col-4"><img width="150px" height="100px" src="images/product-1.jpg" + this.Product[1].img + '">;
-            sp = this.Product[1].name;
-                sp = this.Product[1].price;
-                sp = </div>
-                sp = </div>
+// class productManagement {
+//     constructor(array) {
+//         this.Product = array;
+//     }
+let a= [new sanPham("Áo phông 1","Quần dài","Áo T-Shirt 2","Giày 2","Áo T-Shirt 3","tất 1","Đồng hồ","Đồng hồ 2","Giày 3","Giày 4","Quần 3")]
+    function showList () {
+        let content = "";
+        for (let i = 0; i < a.length; i++) {
+            content+= "    <div class=\"col-4\">\n" +
+                "      <img src='"+a[i].img+"'>\n" +
+                "      <h4>"+a[i].name+"</h4>\n" +
+                "      <p>"+a[i].price+"</p>\n" +
+                "    </div>"
         }
-
-        document.getElementById("list").innerHTML = sp;
+        document.getElementById("demo").innerHTML = content;
     }
